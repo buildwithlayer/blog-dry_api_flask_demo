d1b24037d442c014b42340b41f06d164891f6193
Lucas Gismondi <lucas@gismondigroup.com>
2023-12-14 10:57:26 -0500

    adds deploy workflow

--- app.py
+++ app.py
@@ -4,4 +4,6 @@ from routes import farmers_bp
 
 app = create_app()
 app.register_blueprint(farmers_bp, url_prefix="/farmers")
-app.run(debug=True, host="0.0.0.0", port=8080)
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=8080)
