681e0a240ed58de45d6f839ef955ee9847a8aa46
GavynWithLayer <gavynp@buildwithlayer.com>
2023-12-15 16:02:03 -0500

    Manually Set Servers

--- config.py
+++ config.py
@@ -24,6 +24,13 @@ def create_app():
     app.config[
         "SQLALCHEMY_DATABASE_URI"
     ] = f"sqlite:///{Path(__file__).parent.absolute()}/database.db"
+    app.config["AUTO_SERVERS"] = False
+    app.servers = [
+        {
+            "name": "Production Server",
+            "url": "https://dry-apiflask-demo-r5uz5svela-uc.a.run.app",
+        },
+    ]
 
     db.init_app(app)
     with app.app_context():
